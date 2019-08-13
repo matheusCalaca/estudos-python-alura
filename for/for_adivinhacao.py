f67bc0c488e5f1f03ffbc6589d5fef8099717e92
matheus calaça <matheusfcalaca@gmail.com>
2019-08-12 21:17:42 -0300

    break and continue

--- for/for_adivinhacao.py
+++ for/for_adivinhacao.py
@@ -17,6 +17,11 @@ for rodada in range(rodada, numero_tentativas + 1):
     # converter de str para int
     chute = int(chute_str)
 
+    if(chute < 1 or chute > 100):
+        print("o chute tem que ser de 1 a 100")
+        # continue serve para continuar
+        continue
+
     # variaveies de comparação
     chute_menor = numero_secreto < chute
     chute_maior = numero_secreto > chute
@@ -26,6 +31,8 @@ for rodada in range(rodada, numero_tentativas + 1):
     if (acerto_numero_secreto):
         # para o python apos o IF voce tem que dar 4 espaços para escrever a condicional, se ficar grudado no inicio do arquivo da falha
         print("Você ACERTO o numero secreto: ", numero_secreto)
+        # para o laço
+        break
     else:
         # para se fazer um else if em python usa se  "ELIF"
         if (chute_maior):
