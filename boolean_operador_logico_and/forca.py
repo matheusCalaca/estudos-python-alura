a159e0af9bbaa889873f7b15bc58c85cca608c5c
matheus calaça <matheusfcalaca@gmail.com>
2019-08-15 20:53:37 -0300

    adicionado variaveis booleanas e condicionais not e and

--- boolean_operador_logico_and/forca.py
+++ boolean_operador_logico_and/forca.py
@@ -5,7 +5,16 @@ def jogar():
     print("***Bem vindo ao jogo de Forca***")
     print("********************************")
 
+#variaveis
+palavra_secrta = "banana"
+# definindo uma variavel  para ver se a forca acabou
+# essa variavel e booleana pode ser "True" ou "False"
+enforcou = False
+acertou = False
 
+# para negar se usa a palavra "not" e para a condiciona usa se a palavra "and"
+while(not enforcou and not acertou):
+    print("Jogando")
 
 
 
